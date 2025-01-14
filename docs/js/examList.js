00ca9b9592ba20b252e4aada874ebd775dc60df4
edbpede <144238562+edbpede@users.noreply.github.com>
2025-01-14 12:30:51 +0100

    split files

---
+++ docs/js/examList.js
@@ -0,0 +1,29 @@
+// List of all possible subjects
+export const subjects = [
+	"Biologi",
+	"Dansk", 
+	"Matematik",
+	"Engelsk",
+	"Tysk",
+	"Fransk",
+	"Geografi",
+	"Fysik/kemi"
+];
+
+// Mapping of subjects to their exam folders
+export const examData = {
+	"Dansk": [
+		{
+			name: "Skriftlig Fremstilling",
+			date: "2023-05-02",
+			path: "proever/FP9_dansk/2023-05-02_Skriftlig_Fremstilling/index.html"
+		}
+	],
+	"Matematik": [
+		{
+			name: "Med Hjælpemidler",
+			date: "2023-12-04",
+			path: "proever/FP9_matematik/2023-12-04_Med_Hjaelpemidler/index.html"
+		}
+	]
+};
--- docs/js/landing.js
+++ docs/js/landing.js
@@ -1,3 +1,5 @@
+import { subjects, examData } from './examList.js';
+
 // Date formatting function
 function formatDate(dateString) {
     const [year, month] = dateString.split('-');
@@ -8,51 +10,22 @@ function formatDate(dateString) {
     return `${year} ${monthNames[month] || ''}`;
 }
 
-// List of all possible subjects
-const subjects = [
-    "Biologi",
-    "Dansk", 
-    "Matematik",
-    "Engelsk",
-    "Tysk",
-    "Fransk",
-    "Geografi",
-    "Fysik/kemi"
-];
-
-// Mapping of subjects to their exam folders
-const examData = {
-    "Dansk": [
-        {
-            name: "Skriftlig Fremstilling",
-            date: "2023-05-02",
-            path: "proever/FP9_dansk/2023-05-02_Skriftlig_Fremstilling/index.html"
-        }
-    ],
-    "Matematik": [
-        {
-            name: "Med Hjælpemidler",
-            date: "2023-12-04",
-            path: "proever/FP9_matematik/2023-12-04_Med_Hjaelpemidler/index.html"
-        }
-    ]
-};
-
 // Get the grid container
+
 const grid = document.querySelector('.subject-grid');
 
 // Function to create a subject card
 function createSubjectCard(subject) {
     const card = document.createElement('div');
     card.className = 'subject-card';
-    
+
     const title = document.createElement('h2');
     title.textContent = subject;
     card.appendChild(title);
-    
+
     const examList = document.createElement('ul');
     examList.className = 'exam-list';
-    
+
     if (examData[subject]) {
         examData[subject].forEach(exam => {
             const listItem = document.createElement('li');
@@ -68,7 +41,7 @@ function createSubjectCard(subject) {
         noExams.textContent = 'Ingen eksamener tilgængelige endnu';
         examList.appendChild(noExams);
     }
-    
+
     card.appendChild(examList);
     return card;
 }
